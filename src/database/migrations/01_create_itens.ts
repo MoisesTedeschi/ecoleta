a39b09d0fffee5a2c853f1dce9c0237104acf5ba
Moisés Tedeschi <moisestedeschi@gmail.com>
2020-06-03 02:17:04 -0300

    Tabelas do banco criadas com sucesso.

--- src/database/migrations/01_create_itens.ts
+++ src/database/migrations/01_create_itens.ts
@@ -1,3 +1,15 @@
-export async function up() { }
+import Knex from 'knex';
 
-export async function down() { }
+export async function up(knex: Knex) {
+    // Função para criar tabelas.
+    return knex.schema.createTable('itens', table => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+    });
+}
+
+export async function down(knex: Knex) {
+    // Função para voltar ou "Deletar Tabela Criada".
+    return knex.schema.dropTable('itens')
+}
