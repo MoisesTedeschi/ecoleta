c8cdffe349019874ef7a077f4d5916d3de60e500
Moisés Tedeschi <moisestedeschi@gmail.com>
2020-06-04 14:19:44 -0300

    Atualizando arquivo de rotas.

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import knex from './database/connections';
+
 
 import PointsController from './controllers/PointsController';
 import ItensCotroller from './controllers/ItensController';
