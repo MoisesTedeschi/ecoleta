7af7282e530d27edc315855f212d3ae7d0000f6f
Moisés Tedeschi <moisestedeschi@gmail.com>
2020-06-08 00:51:32 -0300

    Fim do projeto!

--- knexfile.ts
+++ knexfile.ts
@@ -4,13 +4,13 @@ import path from 'path';
 module.exports = {
     client: 'sqlite3',
     connection: {
-        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+        filename: path.resolve(__dirname, 'server', 'database', 'database.sqlite'),
     },
     migrations: {
-        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+        directory: path.resolve(__dirname, 'server', 'database', 'migrations')
     },
     seeds: {
-        directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+        directory: path.resolve(__dirname, 'server', 'database', 'seeds')
     },
     useNullAsDefault: true
 };
